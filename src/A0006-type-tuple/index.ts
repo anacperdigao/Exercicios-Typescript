6369a7a4bc4bd544b8376987249a62aa38c04ac1
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-12-11 10:57:09 -0300

    Read only

--- src/A0006-type-tuple/index.ts
+++ src/A0006-type-tuple/index.ts
@@ -1,5 +1,11 @@
 // É um array com tipo e tamanho fixo
 
+//Caso 1: tamanho fixo
 const dadosCliente: [number, string] = [1, 'Luiz'];
 
 console.log(dadosCliente);
+
+//Caso 2: tamanho variável porque o ultimo parâmetro é opcional
+const dadosCliente2: [number, string, string?] = [1, 'Luiz', 'Miranda'];
+
+console.log(dadosCliente2);
---
+++ src/Aula-extra-read-only/index.ts
@@ -0,0 +1,8 @@
+const array1: readonly string[] = ['Luiz', 'Otavio'];
+const array2: ReadonlyArray<string> = ['Ana', 'Carolina'];
+
+console.log(array1);
+console.log(array2);
+
+// Não consigo modificar nada no array
+//console.log(array1.pop())
